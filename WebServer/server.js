921615a305a381b8858f8bfc9fb73578b9f1f28d
Littlewhite <964873559@qq.com>
2016-05-26 00:49:07 +0800

    WebServer

---
+++ WebServer/server.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+//创建服务器
+http.createServer(function (request,response) {
+	// 解析请求
+	var pathname = url.parse(request.url).pathname;
+
+	//输出请求的文件名
+	console.log('Request for '+ pathname + ' received.');
+
+	//从文件系统中读取请求的文件内容
+	fs.readFile('WebServer/' + pathname.substr(1),function (err,data) {
+		if(err) {
+			console.log(err);
+			response.writeHead(404,{'Content-Type':'text/html'});
+		}else {
+			response.writeHead(200,{'Content-Type':'text/html'});
+
+			//响应文件内容
+			response.write(data.toString());
+		}
+
+		//发送响应数据
+		response.end();
+	});
+}).listen(2333);
+
+//控制台输出信息
+console.log('Server running at http://127.0.0.1:2333');
+
+
+
